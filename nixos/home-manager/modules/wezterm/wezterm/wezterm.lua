c10056c8ea73b79d996196e218292ec56d92ebdd
Abreu <abreu@disroot.org>
2025-01-22 17:27:14 -0300

    fix: Replaced broken require request

--- nixos/home-manager/modules/wezterm/wezterm/wezterm.lua
+++ nixos/home-manager/modules/wezterm/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 local appearance = require("appearance")
-local overrides = require("keymaps")
+local overrides = require("overrides")
 local rendering = require("rendering")
 local config = wezterm.config_builder()
 
