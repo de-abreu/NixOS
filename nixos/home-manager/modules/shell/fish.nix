2938cccd0dae10ce0af94fd091fed721858c217a
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-01-28 10:33:46 -0300

    feat: Added fd

--- nixos/home-manager/modules/shell/fish.nix
+++ nixos/home-manager/modules/shell/fish.nix
@@ -50,8 +50,9 @@
       shellInit =
         # fish
         ''
-          set --universal pure_color_success green
           fish_config theme choose "ayu Mirage"
+          set --universal pure_color_success green
+          set --universal pure_enable_nixdevshell true
         '';
     };
 
