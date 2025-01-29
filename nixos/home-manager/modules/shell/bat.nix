ed47040b4dc203ae610ea5f9f55a0bf3978019f0
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-01-29 19:42:00 -0300

    feat: Added command rg and keymappings to bat

--- nixos/home-manager/modules/shell/bat.nix
+++ nixos/home-manager/modules/shell/bat.nix
@@ -3,4 +3,11 @@
     bat.enable = true;
     fish.shellAbbrs.cat = "bat -p";
   };
+
+  # INFO: Modify key mappings
+  home.file.".lesskey".text = ''
+    j invalid
+    k forw-line
+    l back-line
+  '';
 }
--- nixos/home-manager/modules/shell/utils.nix
+++ nixos/home-manager/modules/shell/utils.nix
@@ -1,10 +1,13 @@
 # INFO: A bunch of useful terminal applications
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    imagemagick # Image manipulation
     bottom # Process monitor
     tig # Visual interface for git
+    ripgrep # search for text within various files
 
     # Requirements to fetch, compress, and uncompress files
+    wget
     curl
     gnutar
     gzip
