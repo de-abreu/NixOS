4a5b6c254b470a0883f981c222394cf439d7c2c8
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-02-02 17:24:40 -0300

    wip: refactored Hyprland configuration

--- nixos/home-manager/modules/desktop/Hyprland/default.nix
+++ nixos/home-manager/modules/desktop/Hyprland/default.nix
@@ -1,16 +1,48 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-with lib; let
-  currentPath = "${config.pathToModules}/desktop/Hyprland/";
-in {
+# TODO: Move the symlinked configuration to nix files once config is finished
+{pkgs, ...}: {
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = false;
-    settings."source" = map (path: "modules/" + (baseNameOf (toString path))) (fileset.toList ./modules);
+    settings = with pkgs; {
+      exec-once = [
+        "dunst"
+        "systemctl --user enable --now hyprpolkitagent.service"
+        "waybar"
+        "wl-paste --type text --watch cliphist store"
+        "wl-paste --type image --watch cliphist store"
+      ];
+      input = {
+        kb_layout = "br";
+        touchpad.natural_scroll = true;
+        sensitivity = 1;
+      };
+      monitor = ["eDP-1,preferred, auto, 1" ",prefered, auto, 1, mirror, eDP-1"];
+      windowrule =
+        [
+          "opacity 0.8, wezterm"
+        ]
+        ++ (map (pattern: "float, title:^${pattern}$$") [
+          "Choose Files"
+          "Save as"
+          "Confirm to replace files"
+          "File Operation Progress"
+        ]);
+      windowrulev2 = "float, class:^(xdg-desktop-portal-gtk)$$";
+
+      # Default applications
+      "$term" = "${wezterm}";
+      "$editor" = "${neovim}";
+      "$file" = "${nautilus}";
+      "$browser" = "${mullvad-browser}";
+
+      # Aesthetics
+      decoration = {
+        blur = 0.8;
+        rounding = 10;
+      };
+      bezier = "easeInSine, 0.12, 0, 0.39, 0";
+      animation = "border, true, 5, easeInSine";
+    };
   };
 
   home.packages = with pkgs; [
@@ -20,14 +52,4 @@ in {
     playerctl # Command-line utility to control media players
     libnotify # Send desktop notifications to the notification daemon
   ];
-
-  xdg.configFile = {
-    "hypr/modules".source = config.lib.file.mkOutOfStoreSymlink "${currentPath}/modules";
-    "hypr/colors.conf".text = with config.lib.stylix; let
-      bases = map (count: "base0" + (toHexString count)) (range 0 15);
-    in
-      concatStringsSep "\n" (
-        map (base: "\$${base} = rgb(${getAttr base colors})") bases
-      );
-  };
 }
---
+++ nixos/home-manager/modules/desktop/Hyprland/keybinds.nix
@@ -0,0 +1,111 @@
+# TODO: Enable notifications, brightness controls, screenshots and screencaptures
+{
+  config,
+  lib,
+  ...
+}: with lib config.lib.stylix.colors; let
+  moveWindows = (concatStringSep "\n" (map (n: replaceStrings ["N"] [(toString n)] ''
+      bind =, N, movetoworkspace, N
+      bind =, N, exec, hyprctl keyword general:col.active_border "rgb(${base08})"
+      bind =, N, submap, hyprmode
+  ''))) + ''
+    bind =, 0, movetoworkspace, 10
+    bind =, 0, exec, hyprctl keyword general:col.active_border "rgb(${base08})"
+    bind =, 0, submap, hyprmode
+  ''
+in {
+  wayland.windowManager.hyprland.extraConfig =
+    ''
+      # █▄▀ █▀▀ █▄█ █▄▄ █ █▄░█ █▀▄ █ █▄░█ █▀▀ █▀
+      # █░█ ██▄ ░█░ █▄█ █ █░▀█ █▄▀ █ █░▀█ █▄█ ▄█
+
+      
+      $ccedilla = code:47 # Map c cedilla for ABNT2 keyboards
+
+      # Default mode keybindings
+      submap = reset
+
+      ## Media controls
+      binddl = , XF86AudioMute, Toggle audio mute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      binddl = , XF86AudioNext, Next media, exec, playerctl next
+      binddl = , XF86AudioPlay, Play media, exec, playerctl play-pause
+      binddl = , XF86AudioPrev, Previous media, exec, playerctl previous
+      bindedl = , XF86AudioLowerVolume, Lower volume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+      bindedl = , XF86AudioRaiseVolume, Raise volume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+
+      # TODO: Enable notifications
+
+      ## TODO: Brightness controls
+
+      ## TODO: Screenshot/Screencapture
+
+      # Hyprmode keybindings
+      bind = SUPER, Super_L, exec, hyprctl keyword general:col.active_border "rgb(${base08})"
+      bindd = SUPER, Super_L, Enter Hyprmode, submap, hyprmode
+      submap = hyprmode
+
+      ## Application shortcuts
+      bindd = , t, Open Terminal, exec, $term
+      bindd = , f, Open File Manager, exec, $file
+      bindd = , ENTER, Open Editor, exec, $editor
+      bindd = , b, Open Clearnet Broswer, exec, $browser
+
+      ## Window/Session controls
+      bindd = , c, Close Window, killactive
+      bindd = , y, Toogle floating window, togglefloating 
+      bindd = , z, Toggle maximize window, fullscreen
+      bind = , z, submap, reset
+      bindd = , q, Lock Screen, exec, swaylock
+      bind = , q, submap, reset
+
+      binded = , j, Focus left window, movefocus, l
+      binded = , k, Focus window below, movefocus, d
+      binded = , l, Focus window above, movefocus, u
+      binded = , $ccedilla, Focus right window, movefocus, r
+
+      binded = ALT, j, Resize left, resizeactive, -30 0
+      binded = ALT, k, Resize down, resizeactive, 0 30
+      binded = ALT, l, Resize up, resizeactive, 0 -30
+      binded = ALT, $ccedilla, Resize right, resizeactive, 30 0
+
+      binded = SHIFT, j, Swap with left window, swapwindow, l
+      binded = SHIFT, k, Swap with window below, swapwindow, k
+      binded = SHIFT, l, Swap with window above, swapwindow, u
+      binded = SHIFT, $ccedilla, Swap with right window, swapwindow, r
+
+      ## Switch workspaces''
+      + (concatStringSep "\n" (map (n: replaceStrings ["N"] [(toString n)] "bind = , N, workspace, N") (range 1 9)))
+      + ''
+      bind = , 0, workspace, 10
+
+      bindd = CTRL, j, Go one workspace left, workspace, -1
+      bindd = CTRL, $ccedilla, Go one workspace right, workspace, +1
+
+      ## Exit Hyprmode 
+      bind = , m, exec, hyprctl keyword general:col.active_border "rgb(${base0B})"
+      bindd = , m, Move focused window to a workspace, submap, MoveWindowToWorkspace
+      bind = SUPER, Super_L, exec, hyprctl keyword general:col.active_border "rgb(${base0D})"
+      bindd = SUPER, Super_L, Exit Hyprmode, submap, reset
+      bind = ,catchall, exec,
+
+      submap =  MoveWindowToWorkspace
+      ''
+      + moveWindows
+      + ''
+      ## Move focused window to a relative workspace
+      binded = , j , Move window to previous workspace, movetoworkspace, -1
+      binded = , $ccedilla , Move window to next workspace, movetoworkspace, +1
+
+      bindd = , s, Silently Move focused window to a workspace, submap, SilentlyMoveWindowToWorkspace
+      bind = , catchall, exec, hyprctl keyword general:col.active_border "rgb(${base0D})"; hyprctl dispatch submap reset
+      # bindd = , catchall, Cancel and return to default mode, submap, reset
+
+      ### Move focused window to another workspace, silently
+      submap = SilentlyMoveWindowToWorkspace
+      ''
+      + moveWindows
+      + ''
+      bind = , catchall, exec, hyprctl keyword general:col.active_border "rgb(${base0D})"; hyprctl dispatch submap reset
+      submap = reset
+    '';
+}
