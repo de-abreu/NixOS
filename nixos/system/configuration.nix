711bd47a502752bf9dbc23300b40bacf61161992
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-02-06 14:42:11 -0300

    feat: Added the pipe operator.

--- nixos/system/configuration.nix
+++ nixos/system/configuration.nix
@@ -16,13 +16,19 @@ with userPrefs; {
     ]
     ++ modules;
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = ["nix-command" "flakes" "pipe-operators"];
 
   users.users."${username}" = {
     isNormalUser = true;
     uid = 1000;
     description = "${username}";
     extraGroups = ["networkmanager" "wheel"];
+  };
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = locale.default;
+    extraLocaleSettings = locale.extraSettings;
   };
 
   # Configure keymap in X11
@@ -57,21 +63,6 @@ with userPrefs; {
 
   # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
