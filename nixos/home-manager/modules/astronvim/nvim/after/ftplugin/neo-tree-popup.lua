76bee7f2f5cca81bd3bbb56db9dc96200affd996
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-02-17 15:50:03 -0300

    feat: Added the oil plugin, using mehalter's configuration

--- nixos/home-manager/modules/astronvim/nvim/after/ftplugin/neo-tree-popup.lua
+++
@@ -1,1 +0,0 @@
-no_hard_wrap.lua
--- nixos/home-manager/modules/astronvim/nvim/after/ftplugin/no_hard_wrap.lua
+++
@@ -1,6 +0,0 @@
--- INFO: Remove the hard wrap and its indicator from pure text, non-modifiable
--- buffers and markup languages. Also remove that weird flickering bug. See:
--- https://github.com/AstroNvim/AstroNvim/issues/1124
-
-vim.opt_local.textwidth = 0
--- vim.opt_local.cmdheight = 1
