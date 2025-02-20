11d67ff00a8cf74b9bfa03ccf00e0a0d535e5789
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-02-20 15:17:16 -0300

    feat: enabled Chromium's uBlock and Bypass-Paywall extensions

--- nixos/home-manager/modules/browsing.nix
+++ nixos/home-manager/modules/browsing.nix
@@ -3,6 +3,16 @@ with pkgs; {
   programs.chromium = {
     enable = true;
     dictionaries = [hunspellDictsChromium.en_US];
+    extensions = [
+      # uBlock Origin
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }
+
+      # Bypass Paywalls
+      {
+        id = "dcpihecpambacapedldabdbpakmachpb";
+        updateUrl = "https://raw.githubusercontent.com/iamadamdev/bypass-paywalls-chrome/master/updates.xml";
+      }
+    ];
   };
   home.packages = [tor-browser mullvad-browser];
 }
