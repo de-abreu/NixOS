bfc44dd9ffd05e2e07f75fc090624b60520a5150
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-02-20 15:15:20 -0300

    code: removed hard wrapping capabilities frim virt-column

--- nixos/home-manager/modules/astronvim/nvim/lua/plugins/editing/virt-column.lua
+++ nixos/home-manager/modules/astronvim/nvim/lua/plugins/editing/virt-column.lua
@@ -3,33 +3,11 @@
 
 ---@type LazySpec
 return {
-	"lukas-reineke/virt-column.nvim",
-	specs = {
-		{
-			"AstroNvim/astrocore",
-			-- Automatically break commentaries at the 80 characters mark.
-			opts = {
-				autocmds = {
-					auto_textwidth = {
-						{
-							event = "BufWinEnter",
-							desc = "Set virtcolumn solelly to buffers",
-							callback = function(args)
-								local buffer = require "astrocore.buffer"
-								if buffer.is_valid(args.buf) then
-									vim.opt_local.textwidth = 80
-								end
-							end,
-						},
-					},
-				},
-			},
-		},
-	},
-	event = "VeryLazy",
-	opts = {
-		char = "🭰",
-		highlight = "Comment",
-		virtcolumn = "+1",
-	},
+  "lukas-reineke/virt-column.nvim",
+  event = "VeryLazy",
+  opts = {
+    char = "🭰",
+    highlight = "Comment",
+    virtcolumn = "80",
+  },
 }
