fd12c7570ff11de975dc49f83f73c3d7b713eaa6
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-02-25 14:04:20 -0300

    fix: weird bug when changing from Hyprland to GNOME

--- nixos/home-manager/modules/terminal/wezterm/modules/multiplexing.lua
+++ nixos/home-manager/modules/terminal/wezterm/modules/multiplexing.lua
@@ -3,17 +3,17 @@ local module = {}
 
 function module.apply_to_config(config)
   -- Attempt to query current workspace
-  local handle, workspace = io.popen "hyprctl activeworkspace", "unix"
-
-  if handle then
-    local output = handle:read "*l"
-    workspace = output:match "workspace ID %d+"
-    handle:close()
-  end
+  -- local handle, workspace = io.popen "hyprctl activeworkspace", "unix"
+  --
+  -- if handle then
+  --   local output = handle:read "*l"
+  --   workspace = output:match "workspace ID %d+"
+  --   handle:close()
+  -- end
 
   -- Create and/or attach to a multiplexer assigned to the current workspace on startup
-  config.unix_domains = { { name = workspace } }
-  config.default_gui_startup_args = { "connect", workspace }
+  config.unix_domains = { { name = "unix" } }
+  config.default_gui_startup_args = { "connect", "unix" }
 end
 
 return module
