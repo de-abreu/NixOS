fa3bebf7aac6bd4f72458a177db79df2a6d43911
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-02-25 14:08:34 -0300

    fix: Missing table error

--- nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/core/none-ls.lua
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/core/none-ls.lua
@@ -7,6 +7,8 @@
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
+    local null_ls = require "null-ls"
+
     -- Only inserts new sources, does not replace existing ones
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
