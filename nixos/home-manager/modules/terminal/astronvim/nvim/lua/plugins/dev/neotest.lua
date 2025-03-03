840f2ecc968f51e2b1090ec305b7e3e55d2f92eb
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-03-03 00:22:41 -0300

    code: more cont.

--- nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/dev/neotest.lua
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/dev/neotest.lua
@@ -1,21 +1,16 @@
+-- TODO: Learn how to use this tool
+
 ---@type LazySpec
 return {
-	"nvim-neotest/neotest",
-	optional = true,
-	dependencies = { "mrcjkb/neotest-haskell", config = function() end },
-	opts = function(_, opts)
-		if not opts.adapters then
-			opts.adapters = {}
-		end
-		table.insert(opts.adapters, require("neotest-haskell")(require("astrocore").plugin_opts("neotest-haskell")))
-	end,
-	keys = {
-		{
-			"<leader>Ts",
-			function()
-				require("neotest").run.stop()
-			end,
-			desc = "Stop test",
-		},
-	},
+  "nvim-neotest/neotest",
+  optional = true,
+  keys = {
+    {
+      "<leader>Ts",
+      function()
+        require("neotest").run.stop()
+      end,
+      desc = "Stop test",
+    },
+  },
 }
