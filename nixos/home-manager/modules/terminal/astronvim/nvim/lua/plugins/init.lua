933690eb9e8ba65ef74bf56d79ad3f660ef79ea1
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-03-03 00:33:20 -0300

    feat: updated plugins and plugin categories

--- nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/init.lua
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/init.lua
@@ -1,8 +1,17 @@
-local categories = { "core", "dev", "editing", "git", "navigation", "ui" }
+local categories = {
+  "core",
+  "dev",
+  "editing",
+  "git",
+  "language-support",
+  "navigation",
+  "ui",
+  "utility",
+}
 local plugins = {}
 
 for _, category in ipairs(categories) do
-	table.insert(plugins, { import = "plugins." .. category })
+  table.insert(plugins, { import = "plugins." .. category })
 end
 --- @type LazySpec
 return plugins
