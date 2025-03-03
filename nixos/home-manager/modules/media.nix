687d531226496ec4950f23eb313fafbb70baa150
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-03-03 00:36:24 -0300

    feat: Configured Zathura

--- nixos/home-manager/modules/media.nix
+++ nixos/home-manager/modules/media.nix
@@ -1,3 +1,3 @@
 {pkgs, ...}: {
-  home.packages = [pkgs.celluloid];
+  home.packages = with pkgs; [celluloid kdePackages.kdenlive];
 }
--- nixos/home-manager/modules/study.nix
+++ nixos/home-manager/modules/study.nix
@@ -1,4 +1,23 @@
+# INFO: Installing study related programs.
 {pkgs, ...}: {
-  home.packages = with pkgs; [anki evince libreoffice-still zotero];
-  xdg.mimeApps.defaultApplications."application/pdf" = "evince.desktop";
+  home.packages = with pkgs; [anki libreoffice-still zotero];
+
+  programs = {
+    # The zotero extension for Chromium
+    chromium.extensions = [{id = "ekhagklcjbdpajgpjgmbionohlpdbjgc";}];
+    zathura = {
+      enable = true;
+      mappings = {
+        K = "navigate next";
+        L = "navigate previous";
+        j = "scroll left";
+        k = "scroll down";
+        l = "scroll up";
+        "ç" = "scroll right";
+      };
+      options.selection-clipboard = "clipboard"; # Enables copying text to clipboard
+    };
+  };
+  xdg.mimeApps.defaultApplications."application/pdf" = "org.pwmt.zathura.desktop";
+  wayland.windowManager.hyprland.settings.windowrule = ["opacity 0.8, zathura"];
 }
