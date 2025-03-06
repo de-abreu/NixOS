2339f0a42c8531fb3ba34c679451501f121360ba
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-03-06 14:32:56 -0300

    feat: improved functionality of virt-column plugin

--- nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/editing/virt-column.lua
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/editing/virt-column.lua
@@ -1,13 +1,44 @@
--- INFO: Set a maximum line length to hardwrap commentaries, and place a
--- vertical marker to indicate it.
+-- INFO: Set a maximum line length of 80 to hardwrap commentaries when editing
+-- code, and place a vertical marker to indicate it.
 
 ---@type LazySpec
 return {
   "lukas-reineke/virt-column.nvim",
   event = "VeryLazy",
+  dependencies = {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      autocmds = {
+        auto_textwidth = {
+          {
+            event = "BufWinEnter",
+            desc = "Set a textwidth limit only for code",
+            callback = function()
+              local filetypes = {
+                "markdown",
+                "plaintex",
+                "quarto",
+                "norg",
+                "tex",
+                "text",
+                "typst",
+              }
+              for _, ft in ipairs(filetypes) do
+                if ft == vim.bo.filetype then
+                  return
+                end
+              end
+              vim.opt_local.textwidth = 80
+            end,
+          },
+        },
+      },
+    },
+  },
   opts = {
     char = "🭰",
     highlight = "Comment",
-    virtcolumn = "80",
+    virtcolumn = "+1",
   },
 }
