b1814824e569dbc26e5afe7de7a60b81f94b64ef
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-03-17 13:47:32 -0300

    feat: Added support for sql language

--- nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/language-support/community.lua
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/language-support/community.lua
@@ -1,13 +1,11 @@
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.fish" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.mdx" },
   { import = "astrocommunity.pack.python-ruff" },
-  { import = "astrocommunity.pack.quarto" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.yaml" },
 }
---
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/language-support/quarto.lua
@@ -0,0 +1,11 @@
+return {
+  { "AstroNvim/astrocommunity", { import = "astrocommunity.pack.quarto" } },
+  {
+    "jmbuhr/otter.nvim",
+    opts = {
+      buffers = {
+        set_filetype = true,
+      },
+    },
+  },
+}
