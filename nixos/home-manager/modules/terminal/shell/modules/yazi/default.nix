adfae384575fd1f8af2889ac47ddbd293ed79ca4
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-04-02 21:14:37 -0300

    feat: Installed and configured yazi

---
+++ nixos/home-manager/modules/terminal/shell/modules/yazi/default.nix
@@ -0,0 +1,10 @@
+{...}: {
+  programs = {
+    yazi = {
+      enable = true;
+      shellWrapperName = "y";
+    };
+  };
+  home.sessionVariables.FILEBROWSER = "yazi";
+  xdg.mimeApps.defaultApplications."inode/directory" = "yazi.desktop";
+}
