3967eca9b0ba4787f8f6326d07da0edd2f57383f
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-04-02 21:18:09 -0300

    feat: Added a grapfical disk utility for the cli

--- nixos/home-manager/modules/terminal/shell/default.nix
+++ nixos/home-manager/modules/terminal/shell/default.nix
@@ -1,20 +1,19 @@
 {
-  filter,
+  includeAll,
   pkgs,
   ...
 }: {
-  imports = filter ./modules;
+  imports = includeAll ./modules;
 
   # General utility programs
   config = {
     home.packages = with pkgs; [
       comma # temporarily install programs to run commands
+      gdu # disk usage analyzer
       imagemagick # Image manipulation
       lynx # terminal based browser
       pb_cli # Output to a pastebin
-      renameutils # Batch file renaming utility
       ripgrep # search for text within various files
-      sqlite
 
       # Requirements to fetch, compress, and uncompress files
       wget
--- nixos/home-manager/modules/terminal/shell/modules/nh.nix
+++ nixos/home-manager/modules/terminal/shell/modules/nh.nix
@@ -5,6 +5,9 @@
       enable = true;
       flake = userPrefs.flakePath;
     };
-    fish.shellAbbrs.nos = "nh os switch -- --show-trace";
+    fish.shellAbbrs = {
+      nd = "nix develop";
+      nos = "nh os switch -- --show-trace";
+    };
   };
 }
