aabe99197f16593732fb51b8fa7c05cf20569f0f
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-04-16 20:07:19 -0300

    feat(main): Mapped media mimetypes to celluloid

--- nixos/home-manager/modules/desktop/media.nix
+++ nixos/home-manager/modules/desktop/media.nix
@@ -1,3 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = [pkgs.celluloid];
+  xdg.mimeApps.defaultApplications =
+    lib.fold
+    (n: acc: {"video/${n}" = "celluloid.desktop";} // acc) {} [
+      "x-matroska"
+      "mp4"
+    ];
 }
