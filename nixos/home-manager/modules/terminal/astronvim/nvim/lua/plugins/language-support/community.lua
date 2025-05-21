33d147d85d89eb2e8376b2f3e3c6e3638f92bb4e
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-05-21 11:37:32 -0300

    feat(main): Added support for C and C++ using clang as language server and formatter.

--- nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/language-support/community.lua
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/language-support/community.lua
@@ -1,10 +1,10 @@
 ---@type LazySpec
 return {
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.fish" },
-  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.mdx" },
   { import = "astrocommunity.pack.python-ruff" },
