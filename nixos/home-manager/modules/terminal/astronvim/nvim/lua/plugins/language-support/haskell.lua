d4b2d4db88eeda0d03d59b6462c18ff4aa2efb71
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-05-21 11:47:02 -0300

    fix(main): call for unexisting key in haskell language configuration

--- nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/language-support/haskell.lua
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/language-support/haskell.lua
@@ -11,7 +11,7 @@ return {
     "AstroNvim/astrolsp",
     opts = function(_, opts)
       local unique = require("astrocore").list_insert_unique
-      opts.servers = unique(opts.sources, { "hls" })
+      opts.servers = unique(opts.servers, { "hls" })
     end,
   },
   ---@type LazyPluginSpec
