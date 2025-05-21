56d622a2218e9d8f7ed4fd9421a064a04c9b2b98
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-05-21 11:51:35 -0300

    refactor(main): Removed some files whose contents were incorporated by others.

--- nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/navigation/nvim-spider.lua
+++ nixos/home-manager/modules/terminal/astronvim/nvim/lua/plugins/navigation/nvim-spider.lua
@@ -2,6 +2,4 @@
 return {
   { import = "astrocommunity.motion.nvim-spider" },
   -- Similar to CamelCaseMotion, but also with some considerations for punctuation
-  { import = "astrocommunity.search.grug-far-nvim" },
-  -- Search and replace
 }
