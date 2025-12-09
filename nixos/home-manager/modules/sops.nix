4ea532c28f9298058fa7cd1ae46272c61094d50f
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-12-09 16:04:51 -0300

    feat(main): First implementation of sops-nix

--- nixos/home-manager/modules/sops.nix
+++
@@ -1,7 +0,0 @@
-{...}: {
-  sops = {
-    age.keyFile = "$XDG_CONFIG_HOME/sops/age/keys.txt";
-    defaultSopsFile = ./sops.yaml;
-  };
-  systemd.user.services.mbsync.Unit.After = ["sops-nix.service"];
-}
