019c7d22c65a344fffb4f2dc5224e499f982c350
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-12-09 11:47:17 -0300

    feat(main): Switched Mullvad to the LibreWolf browser

--- nixos/home-manager/modules/desktop/browsers.nix
+++ nixos/home-manager/modules/desktop/browsers.nix
@@ -1,40 +1,41 @@
-# INFO: Installs GUI based browsers, with Mullvad as the user's default
-
-{pkgs, ...}:
-with pkgs; let
-  default = "mullvad-browser";
-in{
-  programs.chromium = {
+# INFO: Installs GUI based browsers, with Librewolf as the user's default
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  programs.librewolf = {
     enable = true;
-    dictionaries = [hunspellDictsChromium.en_US];
-    extensions = [
-      # uBlock Origin
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }
-
-      # Bypass Paywalls
-      {
-        id = "dcpihecpambacapedldabdbpakmachpb";
-        updateUrl = "https://raw.githubusercontent.com/iamadamdev/bypass-paywalls-chrome/master/updates.xml";
-      }
-    ];
+    languagePacks = ["en-US" "pt-BR"];
+    settings = {
+      "browser.toolbars.bookmarks.visibility" = "never";
+      "browser.translations.neverTranslateLanguages" = "pt";
+      "privacy.resistFingerprinting.letterboxing" = true;
+    };
   };
+  stylix.targets.librewolf.profileNames = ["default"];
   home = {
-    packages = [tor-browser mullvad-browser];
-    sessionVariables.BROWSER = default;
+    packages = [pkgs.tor-browser];
+    sessionVariables.BROWSER = "librewolf";
   };
+  wayland.windowManager.hyprland.settings."$browser" = "librewolf";
   xdg.mimeApps.defaultApplications = let
-    application = default + ".desktop";
-  in map (mime: {name = mime; value = application;}) [
-    "text/html"
-    "x-scheme-handler/http"
-    "x-scheme-handler/https"
-    "x-scheme-handler/chrome"
-    "application/x-extension-htm"
-    "application/x-extension-html"
-    "application/x-extension-shtml"
-    "application/xhtml+xml"
-    "application/x-extension-xhtml"
-    "application/x-extension-xht"
-  ]
-  |> lib.listToAttrs;
+    application = "librewolf.desktop";
+  in
+    map (mime: {
+      name = mime;
+      value = application;
+    }) [
+      "text/html"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+      "x-scheme-handler/chrome"
+      "application/x-extension-htm"
+      "application/x-extension-html"
+      "application/x-extension-shtml"
+      "application/xhtml+xml"
+      "application/x-extension-xhtml"
+      "application/x-extension-xht"
+    ]
+    |> lib.listToAttrs;
 }
