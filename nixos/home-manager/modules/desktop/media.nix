85a1ed28c676749077fc7fa5d72b889939aef528
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-12-09 11:46:49 -0300

    feat(main): Added OBS for screen captures

--- nixos/home-manager/modules/desktop/media.nix
+++ nixos/home-manager/modules/desktop/media.nix
@@ -6,8 +6,9 @@
   home.packages = [pkgs.celluloid];
   xdg.mimeApps.defaultApplications =
     lib.fold
-    (n: acc: {"video/${n}" = "celluloid.desktop";} // acc) {} [
+    (elem: acc: {"video/${elem}" = "celluloid.desktop";} // acc) {} [
       "x-matroska"
       "mp4"
     ];
+  programs.obs-studio.enable = true;
 }
